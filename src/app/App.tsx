65bed1c05bd14e8ff82ff8fcf23d5c853ee461bd
Eva Kerrigan <evakerrigan@gmail.com>
2023-07-01 18:10:05 +0300

    feat: add Header structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,10 @@
 import { AppRouter } from '@/app/providers/router';
+import { Header } from '@/features';
 
 export const App = () => {
   return (
     <div>
+      <Header />
       <AppRouter />
     </div>
   );
